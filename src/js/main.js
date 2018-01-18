0842a43ce750a0b54ecef36571a79ee7e4b54c82
Emma O'Neill <emmaruthoneill@gmail.com>
2018-01-18 13:09:48 -0800

    got the nav working ok

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 require("./lib/social"); //Do not delete
 
-(function blink() { 
-  $('.blink').fadeOut(500).fadeIn(500, blink); 
+(function blink() {
+  $('.blink').fadeOut(500).fadeIn(500, blink);
 })();
 
 (function(d, s, id) {
@@ -20,7 +20,60 @@ $(window).scroll(function () {
   		scrollPercent = (s / (d-c)) * 100;
   var position = scrollPercent;
 
+  // console.log(s);
+  if (s > 0) {
+    $('#fixednav').fadeIn(800)
+    $("#fixednav").css("visibility","visible");
+  } else {
+    $('#fixednav').fadeOut(800)
+    $("#fixednav").css("visibility","hidden");
+  }
+
   $("progress").attr('value', position);
+
+  navDisplay(s);
 
 });
 
+$(document).on('click', 'a[href^="#"]', function(e) {
+    // target element id
+    var id = $(this).attr('href');
+
+    // target element
+    var $id = $(id);
+    if ($id.length === 0) {
+        return;
+    }
+
+    // prevent standard hash navigation (avoid blinking in IE)
+    e.preventDefault();
+
+    // top position relative to the document
+    var pos = $(id).offset().top;
+
+    // animated top scrolling
+    $('body, html').animate({scrollTop: pos});
+});
+
+
+var scrolllist = ["hours12","day1","day2","day3","day4","day5","day6","future"];
+var navIDX = -1;
+var navDisplay = function() {
+
+  var pos = document.body.scrollTop || document.documentElement.scrollTop;
+  var start_top = 30;
+  var pos_map_top = $('#hours12').offset().top-start_top;
+
+  $(".navlink").removeClass("activelink");
+  navIDX = -1;
+  for (var i=0; i<scrolllist.length; i++){
+    var pos_map = $('#'+scrolllist[i]).offset().top - pos - start_top;
+    if (pos_map < 0) {
+      navIDX = i;
+    }
+  }
+  console.log(navIDX);
+  if (navIDX > -1){
+    $("#"+scrolllist[navIDX]+"nav").addClass("activelink");
+  }
+};
